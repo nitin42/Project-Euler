ca96418eaa42e15cbf11a467736bc1c4a34496ed
Nitin Tulswani <tulswani19@gmail.com>
2016-07-30 23:55:12 +0530

    coleifer cache part

---
+++ problem14.py
@@ -0,0 +1,30 @@
+# Thankx to Charles Leifer
+
+def rule(num):
+	if num%2 == 0:
+		return num/2
+	else:
+		return 3*num + 1
+
+dic = {1:1}
+n = 0
+m = 0
+
+for i in range(1, 1000000, 2):
+	res = i
+	max_terms = 0
+	while res!=1:
+		if dic.has_key(res):
+			max_terms += dic[res] # Add only one term
+			break
+		else:
+			max_terms += 1
+			res = rule(res) # Calculate the total terms
+
+	dic[i] = max_terms
+
+	if max_terms>m: # Number of terms
+		n = i
+		m = max_terms
+
+print n
