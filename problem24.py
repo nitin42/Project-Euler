82a523b4411aabf6daa9cafed8471a4e410546ad
Nitin Tulswani <tulswani19@gmail.com>
2016-08-05 19:05:30 +0530

    added itertools

---
+++ problem24.py
@@ -0,0 +1,11 @@
+from itertools import permutations
+
+def permutation():
+	a = list(set(permutations(['0','1','2','3','4','5','6','7','8'])))
+	b = a.sort()
+	c = [''.join(i) for i in a]
+	for i in c:
+		print i
+
+if __name__ == "__main__":
+	permutation()
