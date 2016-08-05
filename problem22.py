653e63632a55da9af44aa4c13dd77d090da22156
Nitin Tulswani <tulswani19@gmail.com>
2016-08-05 17:45:23 +0530

    added score

---
+++ problem22.py
@@ -0,0 +1,19 @@
+def score(name):
+	score = 0
+	for char in name:
+		score += ord(char) - ord('A') + 1 # The score for each character starting from index 1
+	return score
+
+file_obj = open('names.txt', 'r')
+names = file_obj.read() # Read the lines
+names = names.replace('"', '') # Make the names stringify
+names = names.split(',') # Make a list of all the names in the text file
+names.sort()
+
+total_score = 0
+
+for i in range(len(names)):
+	my_score = score(names[i])*(i+1) # Score multiplied by the position of the word
+	total_score += my_score
+
+print total_score
