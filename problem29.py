7b4bffb87fa344c8e88b2acb8a5bd06923c5e70c
Nitin Tulswani <tulswani19@gmail.com>
2016-08-07 01:50:57 +0530

    add set()

---
+++ problem29.py
@@ -0,0 +1,25 @@
+''' 
+
+1. Get the value of a and b from the equation.
+2. Get the value of a**b until the maximum b.
+3. Store the result in the set and it should be sorted accordingly.
+4. Print the result.
+
+'''
+
+def distinct_power():
+	l = []
+	count = 0
+	#a = set()
+	for a in range(2, 101):
+		for b in range(2, 101):
+			l.append(a**b)
+	l.sort()
+
+	for i in set(l):
+		count += 1
+
+	print count
+
+if __name__ == '__main__':
+	distinct_power()
