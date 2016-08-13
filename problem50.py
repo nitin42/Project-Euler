119f519a59ee7b2d42b0e7713e3b800f9e46c3c5
Nitin Tulswani <tulswani19@gmail.com>
2016-08-13 16:09:34 +0530

    may work

---
+++ problem50.py
@@ -0,0 +1,31 @@
+def prime(n):
+	l = []
+	l2 = []
+	for num in range(2, n+1):
+		if num>1:
+			for i in range(2, num):
+				if num%i == 0:
+					break
+			else:
+				l.append(num)
+	
+	for i in l:
+		for j in l[1:]:
+			a = i+j
+			if is_prime(a):
+				l2.append(a) 
+
+	print l2			
+	
+
+def is_prime(n):
+	if n>1:
+		for i in range(2, n):
+			if n%i == 0:
+				return False
+		else:
+			return True
+	else:
+		return False
+
+prime(1000000)
