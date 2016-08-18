2208c7a3f7356bdc89ae4dc8a330e8a4d4877b27
Nitin Tulswani <tulswani19@gmail.com>
2016-08-18 23:12:42 +0530

    test included

---
+++ problem52.py
@@ -0,0 +1,30 @@
+'''
+
+Solution: Just see that the number of digits present in the multiple of 6X are present in X.
+
+'''
+
+# while loop iterator
+
+def multiple():
+	i = 1
+
+	while True:
+		if set(str(i)) == set(str(6*i)):
+			if set(str(i)) == set(str(5*i)):
+				if set(str(i)) == set(str(4*i)):
+					if set(str(i)) == set(str(3*i)):
+						if set(str(i)) == set(str(2*i)):
+							print i
+							break
+		i += 1
+
+
+	'''
+	>>>142857
+	'''
+
+
+if __name__ == '__main__':
+	import doctest 
+	doctest.testmod()
