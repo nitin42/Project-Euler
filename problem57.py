0eaf1bc8c07918c533467ee962908a525cc7d073
Nitin Tulswani <tulswani19@gmail.com>
2016-08-18 23:14:26 +0530

    took a while

---
+++ problem57.py
@@ -0,0 +1,14 @@
+import fractions
+
+def root_convergents(limit):
+	d = fractions.Fraction(1/2) 
+	for i in range(limit):
+		d = fractions.Fraction(1/(2+d)) # This grows until the iterations end.
+		yield 1 + d 
+
+def main():
+	print(len([f for f in root_convergents(1000) if len(str(f.numerator))>
+					len(str(f.denominator))]))
+
+if __name__ == '__main__':
+	main()
