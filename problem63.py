27d1ce700725a6778e4d077ab01beb2ce63e43b5
Nitin Tulswani <tulswani19@gmail.com>
2016-08-18 23:14:53 +0530

    logarithmic equation

---
+++ problem63.py
@@ -0,0 +1,27 @@
+'''
+
+The number of digits in a^b is [D = 1 + Log10(a^b)](floor function)
+Upper bound -> 9^n,
+
+D = 1 + n(Log10(9))
+
+
+'''
+
+from math import log10
+
+def powerful_digit():
+
+	i = 0
+
+	for num in range(1, 10):
+
+		i += int(1 / (1 - log10(num)))
+
+	'''
+	>>>49
+	'''
+
+if __name__ == '__main__':
+	import doctest
+	doctest.testmod()
