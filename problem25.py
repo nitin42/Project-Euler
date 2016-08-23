c20c931f764599f7d11b9b16091c3f1190e5f9ba
Nitin Tulswani <tulswani19@gmail.com>
2016-08-23 15:52:30 +0530

    added doctest

--- problem25.py
+++ problem25.py
@@ -1,10 +1,16 @@
 def fibo(n):
-	a, b = 1, 0
-	count = 0
-	while a<n:
-		a, b = a+b, a
-		count += 1
-	return count+1
+    a, b = 1, 0
+    count = 0
+    while a<n:
+        a, b = a+b, a
+        count += 1
+    return count+1
+
+    '''
+    >>> fibo(int('1%s'%('0'*999)))
+    4782
+    '''
 
 if __name__ == '__main__':
-	print fibo(int('1%s'%('0'*999))) # Because at this number we see our first 1000 digit fibonacci number in series
+    import doctest
+    doctest.testmod()    
