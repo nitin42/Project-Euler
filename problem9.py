94341875e623803a17a6aa781cb01bfbb01ea6cf
Nitin Tulswani <tulswani19@gmail.com>
2016-08-24 00:06:03 +0530

    tested

--- problem9.py
+++ problem9.py
@@ -1,19 +1,27 @@
 def pythagorean():
-	# a+b+c = 1000        ---- (1)
-	# a^2 + b^2 = c^2     ---- (2)
+    # a+b+c = 1000        ---- (1)
+    # a^2 + b^2 = c^2     ---- (2)
 
-	# a = 1000*(500-b) / (1000 - b) ----> After mathematically solving the equation by squaring the eq. 1
+    # a = 1000*(500-b) / (1000 - b) ----> After mathematically solving the equation by squaring the eq. 1
 
-	for b in range(1,500):
-		if 1000*(500-b) % (1000-b) == 0:
-			a = 1000*(500-b) / (1000-b)
-			print b, a
+    '''
+    >>> pythagorean()
+    200 375
+    375 200
+    289001
+    '''
 
-	A = a**2
-	B = b**2
-	C = A+B
+    for b in range(1,500):
+        if 1000*(500-b) % (1000-b) == 0:
+            a = 1000*(500-b) / (1000-b)
+            print b, a
 
-	print C
+    A = a**2
+    B = b**2
+    C = A+B
+
+    print C
 
 if __name__ == '__main__':
-	pythagorean()
+    import doctest
+    doctest.testmod(verbose=True)
