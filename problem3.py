99900626d20821baf2addb1332c322b46268d6be
Nitin Tulswani <tulswani19@gmail.com>
2016-08-23 16:12:46 +0530

    proper tests

--- problem3.py
+++ problem3.py
@@ -1,24 +1,34 @@
 from math import sqrt
 
 def max_prime(n):
-	if n<0:
-		return False
-	for i in range(2, sqrt(n)+1):
-		if n%i == 0:
-			return False
+    if n<0:
+        return False
+    for i in range(2, n):
+        if n%i == 0:
+            return False
 
-	return True
+    return True
 
-i = 1 # Initially
-number = 600851475143
+def check():
 
-# After this n must be odd.
-while i*i<=number:
-	i += 2
-	if number%i == 0:
-		if max_prime(i)
-			print i
+    '''
+    >>> check()
+    6857
+    '''
 
+    i = 1 # Initially
+    number = 600851475143
+    l = []
+
+    # After this n must be odd.
+    while i*i<=number:
+        i += 2
+        if number%i == 0:
+            if max_prime(i):
+                l.append(i)
+
+    print max(l)
 
 if __name__ == '__main__':
-	prime(int(raw_input()))
+    import doctest
+    doctest.testmod()
