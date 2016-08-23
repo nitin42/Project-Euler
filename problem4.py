bd01ff35193fb623846ce2e87719cdaa28844029
Nitin Tulswani <tulswani19@gmail.com>
2016-08-23 23:07:50 +0530

    tested

--- problem4.py
+++ problem4.py
@@ -1,13 +1,21 @@
 def palindrome(n):
-	return str(n)[::] == str(n)[::-1]
+    return str(n)[::] == str(n)[::-1]
 
-m = 0
-while x>=100:
-	while y>=100:
-		mul = x*y
-		if palindrome(mul) and mul>m:
-			m = mul # Maximum product
-		y -= 11 # Largest 3 digit number multiple of 11
-		x -= 1
+def cal_pal():
+    '''
+    >>> cal_pal()
+    2332
+    '''
+    m = 0
+    for i in range(1, 101):
+        for j in range(1, 101):
+            mul = i*j
+            if palindrome(mul) and mul>m:
+                m = mul
+            j -= 11
+            i -= 1
+    print m
 
-print m
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod(verbose=True)
