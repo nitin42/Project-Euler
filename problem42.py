a3825195fe1f2bebc11a0d62987a83ab5f99ffb4
Nitin Tulswani <tulswani19@gmail.com>
2016-08-23 23:21:34 +0530

    tested

--- problem42.py
+++ problem42.py
@@ -1,34 +1,43 @@
 from __future__ import division
 
 def score(name):
-	score = 0
-	for char in name:
-		score += ord(char) - ord('A') + 1 # The score for each character starting from index 1
-	return abs(score)
+    score = 0
+    for char in name:
+        score += ord(char) - ord('A') + 1 # The score for each character starting from index 1
+    return abs(score)
 
-file_obj = open('words.txt', 'r')
-names = file_obj.read()
-names = names.replace('"', '')
-name = names.split(',')
+def cal_score():
+    '''
+    >>> cal_score()
+    847
+    '''
+    file_obj = open('words.txt', 'r')
+    names = file_obj.read()
+    names = names.replace('"', '')
+    name = names.split(',')
 
-l = []
+    l = []
 
-for i in range(len(names)):
-	res = score(names[i]) # Calculate the score for each word in the list
-	l.append(res)
+    for i in range(len(names)):
+        res = score(names[i]) # Calculate the score for each word in the list
+        l.append(res)
 
-l3 = map(int, l) # Map all the scores as integer
+    l3 = map(int, l) # Map all the scores as integer
 
-#print l3
+    #print l3
 
-l2 = []
-count = 0
+    l2 = []
+    count = 0
 
-for i in l3:
-	for j in range(1, 1000000):
-		if i == ((j*j + j)/2): # Check for triangle for numbers
-			count += 1 # If found 
-		else:
-			break
+    for i in l3:
+        for j in range(1, 1000000):
+            if i == ((j*j + j)/2): # Check for triangle for numbers
+                count += 1 # If found 
+            else:
+                break
 
-print count
+    print count
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod(verbose=True)
