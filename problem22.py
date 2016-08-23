51433e673e7f24501a881fc4b6c2b4324b2347f6
Nitin Tulswani <tulswani19@gmail.com>
2016-08-23 15:41:30 +0530

    doctest included

--- problem22.py
+++ problem22.py
@@ -1,19 +1,29 @@
 def score(name):
-	score = 0
-	for char in name:
-		score += ord(char) - ord('A') + 1 # The score for each character starting from index 1
-	return score
+    score = 0
+    for char in name:
+        score += ord(char) - ord('A') + 1 # The score for each character starting from index 1
+    return score
 
-file_obj = open('names.txt', 'r')
-names = file_obj.read() # Read the lines
-names = names.replace('"', '') # Make the names stringify
-names = names.split(',') # Make a list of all the names in the text file
-names.sort()
+def name():
+    file_obj = open('names.txt', 'r')
+    names = file_obj.read() # Read the lines
+    names = names.replace('"', '') # Make the names stringify
+    names = names.split(',') # Make a list of all the names in the text file
+    names.sort()
 
-total_score = 0
+    total_score = 0
 
-for i in range(len(names)):
-	my_score = score(names[i])*(i+1) # Score multiplied by the position of the word
-	total_score += my_score
+    for i in range(len(names)):
+        my_score = score(names[i])*(i+1) # Score multiplied by the position of the word
+        total_score += my_score
 
-print total_score
+    print total_score
+
+    '''
+    >>> name()
+    871198282
+    '''
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
