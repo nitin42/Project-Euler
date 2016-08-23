080d4bc61ffc016a6473f8f287e11ef5df2bbdc8
Nitin Tulswani <tulswani19@gmail.com>
2016-08-23 21:03:51 +0530

    some corrections

--- problem1.py
+++ problem1.py
@@ -1,14 +1,15 @@
 def init_sum():
-	initial_sum = 0
-	for i in range(1000):
-		if i%3 == 0 or i%5 == 0:
-			initial_sum += i
-		
 	'''
 	>>> init_sum()
 	233168
 	'''
+	initial_sum = 0
+	for i in range(1000):
+		if i%3 == 0 or i%5 == 0:
+			initial_sum += i
+	return initial_sum
+		
 	
 if __name__ == '__main__':
 	import doctest
-	doctest.testmod()
+	doctest.testmod(verbose=True)
