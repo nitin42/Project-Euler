bb6ee5c3aa94c9247be90eaadffc5d0d88a85fb0
Nitin Tulswani <tulswani19@gmail.com>
2016-08-23 15:47:15 +0530

    updated with doctest

--- problem23.py
+++ problem23.py
@@ -8,25 +8,36 @@ def divisors(n):
     return set(arr) # Each divisor should be unique
 
 def abundant_sum(n):
-	l = []
-	for i in range(1, n+1):
-		ab_sm = sum(divisors(i))
-		if ab_sm>i: # If the sum is greater than the number itself than the number is abundant
-			l.append(i) # Store that number
-	return l
+    l = []
+    for i in range(1, n+1):
+        ab_sm = sum(divisors(i))
+        if ab_sm>i: # If the sum is greater than the number itself than the number is abundant
+            l.append(i) # Store that number
+    return l
 
-num = abundant_sum(28124) # Some numbers will be abundant and some will be deficient
+def cal_abund():
+    num = abundant_sum(28124) # Some numbers will be abundant and some will be deficient
+    store = set()
 
-store = set()
+    for i in range(len(num)):
+        for j in range(i, len(num)): # Because its sum of TWO abundant numbers
+            n = num[i] + num[j]
+            if n<28124:
+                store.add(n) # Store the number that is abundant
 
-for i in range(len(num)):
-	for j in range(i, len(num)): # Because its sum of TWO abundant numbers
-		n = num[i] + num[j]
-		if n<28124:
-			store.add(n) # Store the number that is abundant
+    ''' 
+    sum of all the positive integers which cannot be written as the sum of two abundant numbers
+    '''
 
-''' 
-sum of all the positive integers which cannot be written as the sum of two abundant numbers
-'''
+    a = sum(set(range(28124)).difference(store))
+    print a
 
-a = sum(set(range(28124)).difference(store))
+    '''
+    >>> cal_abund()
+    4179871
+    '''
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+    
