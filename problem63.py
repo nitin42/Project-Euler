b59d30240bdd4e98edb27563105911931f382531
Nitin Tulswani <tulswani19@gmail.com>
2016-08-24 00:00:14 +0530

    tested

--- problem63.py
+++ problem63.py
@@ -11,17 +11,18 @@ D = 1 + n(Log10(9))
 from math import log10
 
 def powerful_digit():
-
+	
+	'''
+	>>> powerful_digit()
+	49
+	'''
+	
 	i = 0
 
 	for num in range(1, 10):
 
 		i += int(1 / (1 - log10(num)))
 
-	'''
-	>>>49
-	'''
-
 if __name__ == '__main__':
 	import doctest
-	doctest.testmod()
+	doctest.testmod(verbose=True)
