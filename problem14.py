a048d6f510141668c969bd4b2ffe55029a6766a2
Nitin Tulswani <tulswani19@gmail.com>
2016-08-23 14:57:00 +0530

    with doctest

--- problem14.py
+++ problem14.py
@@ -10,21 +10,31 @@ dic = {1:1}
 n = 0
 m = 0
 
-for i in range(1, 1000000, 2):
-	res = i
-	max_terms = 0
-	while res!=1:
-		if dic.has_key(res):
-			max_terms += dic[res] # Add only one term
-			break
-		else:
-			max_terms += 1
-			res = rule(res) # Calculate the total terms
+def cal():
+	for i in range(1, 1000000, 2):
+		res = i
+		max_terms = 0
+		while res!=1:
+			if dic.has_key(res):
+				max_terms += dic[res] # Add only one term
+				break
+			else:
+				max_terms += 1
+				res = rule(res) # Calculate the total terms
 
-	dic[i] = max_terms
+		dic[i] = max_terms
 
-	if max_terms>m: # Number of terms
-		n = i
-		m = max_terms
+		if max_terms>m: # Number of terms
+			n = i
+			m = max_terms
 
-print n
+	print n
+
+	'''
+	>>> cal()
+	837799
+	'''
+
+if __name__ == '__main__':
+	import doctest
+	doctest.testmod()
