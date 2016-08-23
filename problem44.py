d72760413d19ff42bc1e6f563e5a6553887c4461
Nitin Tulswani <tulswani19@gmail.com>
2016-08-23 23:24:10 +0530

    tested

--- problem44.py
+++ problem44.py
@@ -4,17 +4,28 @@
 # Calculate the D 
 
 def pentagonal(n):
-	l = []	
-	for i in range(1, n+1):
-		pn = ((3*i*i - i)/2)	
-		l.append(pn)
+    '''
+    >>> pentagonal(10)
+    22 92
+    35 70
+    70 35
+    70 92
+    92 22
+    92 70
+    '''
 
-	for i in l:
-		for j in l:
-			an = i+j
-			sn = abs(i-j)
-			if an and sn in l:
-				print i, j
+    l = []  
+    for i in range(1, n+1):
+        pn = ((3*i*i - i)/2)    
+        l.append(pn)
+
+    for i in l:
+        for j in l:
+            an = i+j
+            sn = abs(i-j)
+            if an and sn in l:
+                print i, j
 
 if __name__ == '__main__':
-	pentagonal(10)
+    import doctest
+    doctest.testmod(verbose=True)
