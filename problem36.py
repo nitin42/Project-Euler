45f19e6f79be1990341dec6664c5bd86a9dfe115
Nitin Tulswani <tulswani19@gmail.com>
2016-08-23 23:02:20 +0530

    tested

--- problem36.py
+++ problem36.py
@@ -1,14 +1,19 @@
 def bin_base(n):
-	l = []
-	for i in range(1, n+1):
-		j = str(i) 
-		a = bin(i) # Store the binary for the number
-		b = a[2:]
-		if b == b[::-1] and j == j[::-1]: # Store the numbers whose base 10 and base 2 are palindrome
-			l.append(i)
+    '''
+    >>> bin_base(1000000)
+    872187
+    '''
 
-	print sum(l) # Print the sum of the numbers
+    l = []
+    for i in range(1, n+1):
+        j = str(i) 
+        a = bin(i) # Store the binary for the number
+        b = a[2:]
+        if b == b[::-1] and j == j[::-1]: # Store the numbers whose base 10 and base 2 are palindrome
+            l.append(i)
+
+    print sum(l) # Print the sum of the numbers
 
 if __name__ == '__main__':
-	bin_base(1000000) # One million numbers 
-
+    import doctest
+    doctest.testmod(verbose=True)
