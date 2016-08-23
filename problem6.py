81d296b5f0b749f34d3ad18ebc6a7787ffe071e2
Nitin Tulswani <tulswani19@gmail.com>
2016-08-23 23:59:32 +0530

    tested

--- problem6.py
+++ problem6.py
@@ -1,17 +1,23 @@
 def sum_square(n):
-	sum_of_sq = 0
-	sq_of_sum = 0
-	for i in range(1, n+1):
-		sum_of_sq += i*i
+    '''
+    >>> sum_square(145)
+    111015480
+    '''
 
-	for j in range(1, n+1):
-		sq_of_sum += j
+    sum_of_sq = 0
+    sq_of_sum = 0
+    for i in range(1, n+1):
+        sum_of_sq += i*i
 
-	square = sq_of_sum*sq_of_sum
+    for j in range(1, n+1):
+        sq_of_sum += j
 
-	final = square - sum_of_sq
+    square = sq_of_sum*sq_of_sum
 
-	print final
+    final = square - sum_of_sq
+
+    print final
 
 if __name__ == '__main__':
-	sum_square(int(raw_input()))
+    import doctest
+    doctest.testmod(verbose=True)
