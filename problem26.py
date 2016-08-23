dab0e182c88769f051cd0e4d1f2577d4a04acdd5
Nitin Tulswani <tulswani19@gmail.com>
2016-08-23 15:59:45 +0530

    with doctest

--- problem26.py
+++ problem26.py
@@ -1,20 +1,26 @@
 def reciprocate_cycle():
-	mx = 0 # For storing the maximum number of the remainders
-	for i in range(7, 1000):
-		count = 0 # Initially there is no remainder so count is zero
-		value = 10 % i # Store the remainder in value variable
-		n = 1000 # For tracking the longest value of d under 1000
-		while(value != 1 and z > 0):
-			value = value * 10
-			value = value % 10
-			count += 1
-			z -= 1 
-		if (count>mx and z>1):
-			mx = count
-			mx2 = i
+    mx = 0 # For storing the maximum number of the remainders
+    for i in range(7, 1000):
+        count = 0 # Initially there is no remainder so count is zero
+        value = 10 % i # Store the remainder in value variable
+        n = 1000 # For tracking the longest value of d under 1000
+        z = 0
+        while(value != 1 and z >= 0):
+            value = value * 10
+            value = value % 10
+            count += 1
+            z -= 1 
+        if (count>mx and z>1):
+            mx = count
+            mx2 = i
 
-	print ("The value of d for which 1/d produces the longest recursive cycle is : %d" % i )
+    print  i
 
+    '''
+    >>> reciprocate_cycle()
+    999
+    ''' 
 
 if __name__ == '__main__':
-	reciprocate_cycle()
+    import doctest
+    doctest.testmod()
