a6e89609c782a127ad1de3e8852f499ffc02741d
Nitin Tulswani <tulswani19@gmail.com>
2016-08-24 00:01:33 +0530

    tested

--- problem7.py
+++ problem7.py
@@ -1,15 +1,19 @@
 def prime(n):
-	l = []
-	for num in range(2, n+1):
-		if num>1:
-			for i in range(2, num):
-				if num%i == 0:
-					break
-			else:
-				l.append(num)
-	print l[1000]
+    '''
+    >>> prime(25000)
+    7927
+    '''
 
-prime(25000)
+    l = []
+    for num in range(2, n+1):
+        if num>1:
+            for i in range(2, num):
+                if num%i == 0:
+                    break
+            else:
+                l.append(num)
+    print l[1000]
 
-#if __name__ == '__main__':
-	#prime(int(raw_input()))
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod(verbose=True)
