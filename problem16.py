4961bff1c3a4baf56e13bd0ad5d053f9542827d7
Nitin Tulswani <tulswani19@gmail.com>
2016-08-23 21:10:15 +0530

    tested

--- problem16.py
+++ problem16.py
@@ -1,14 +1,13 @@
 def power(num):
+	'''
+	>>> power(1000)
+	1366
+	'''
 	a = 2**num
 	b = list(str(a))
 	res = [int(i) for i in b]
 	print sum(res)
 
-	'''
-	>>> power(1000)
-	1366
-	'''
-
 if __name__ == '__main__':
 	import doctest
-	doctest.testmod()
+	doctest.testmod(verbose=True)
