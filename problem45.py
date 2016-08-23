cd1f6a9c306d5e0392c2d728a66e0bd8c09852f2
Nitin Tulswani <tulswani19@gmail.com>
2016-08-23 23:25:38 +0530

    tested

--- problem45.py
+++ problem45.py
@@ -1,29 +1,37 @@
 from __future__ import division
 
 def pent_trian_hex(n):
-	p = []
-	t = []
-	h = []
-	n1 = int(n)
-	for i in range(1, n1+1):
-		pn = (3*i*i - i)/2
-		p.append(pn)
+    '''
+    >>> pent_trian_hex(500)
+    1.0
+    210.0
+    40755.0
+    '''
 
-	for i in range(1, n1+1):
-		tn = (i*i + i)/2
-		t.append(tn)
+    p = []
+    t = []
+    h = []
+    n1 = int(n)
+    for i in range(1, n1+1):
+        pn = (3*i*i - i)/2
+        p.append(pn)
 
-	for i in range(1, n1+1):
-		hn = (2*i*i - i)/2
-		h.append(hn)
+    for i in range(1, n1+1):
+        tn = (i*i + i)/2
+        t.append(tn)
 
-	p1 = map(str, p)
-	t1 = map(str, t)
-	h1 = map(str, h)
+    for i in range(1, n1+1):
+        hn = (2*i*i - i)/2
+        h.append(hn)
 
-	for i in t1:
-		if i in p1 and h1:
-			print i
+    p1 = map(str, p)
+    t1 = map(str, t)
+    h1 = map(str, h)
+
+    for i in t1:
+        if i in p1 and h1:
+            print i
 
 if __name__ == '__main__':
-	pent_trian_hex(500)
+    import doctest
+    doctest.testmod(verbose=True)
