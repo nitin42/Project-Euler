50c63102bbe705115f154efdee3819e013468b87
Nitin Tulswani <tulswani19@gmail.com>
2016-08-23 22:33:35 +0530

    tested

--- problem34.py
+++ problem34.py
@@ -1,31 +1,38 @@
-def factorial(n):
-	f = 1
-	if n < 0:
-		return False
-	else:
-		for i in range(1, n+1):
-			f = f * i
-		return f
-
+def fact(n):
+    "Return n!."
+    if n < 2:
+        return 1
+    else:
+        result = 1
+        for i in xrange (2, n + 1):
+            result = result * i
+        return result
 
 def curious_number():
-	s = 0
-	r = 0
-	'''
-	upper bound is on 9!*7 = 2540160 which is a seven digit number
+    '''
+    >>> curious_number()
+    40730
+    '''
 
-	'''
-	for i in range(10, 2540161):
-		n = i
-		while(n>0):
-			d = n%10
-			n /= 10
-			s += factorial(d)
+    #s = 0
+    r = 0
+    '''
+    upper bound is on 9!*7 = 2540160 which is a seven digit number
 
-		if (s == i):
-			r += i
+    '''
+    for i in range(10, 2540161):
+        n = i
+        s = 0
+        while(n>0):
+            d = n%10
+            n /= 10
+            s += fact(d)
 
-	print r
+        if (s == i):
+            r += i
+
+    print r
 
 if __name__ == '__main__':
-	curious_number()
+    import doctest
+    doctest.testmod(verbose=True)
