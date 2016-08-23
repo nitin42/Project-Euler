86c229fa9a1330a27895cdceca1c521fae6bd3e9
Nitin Tulswani <tulswani19@gmail.com>
2016-08-23 14:05:12 +0530

    added doctest

--- problem1.py
+++ problem1.py
@@ -1,7 +1,14 @@
-initial_sum = 0
-
-for i in range(1000):
-	if i%3 == 0 or i%5 == 0:
-		initial_sum += i
-
-print initial_sum
+def init_sum():
+	initial_sum = 0
+	for i in range(1000):
+		if i%3 == 0 or i%5 == 0:
+			initial_sum += i
+		
+	'''
+	>>> init_sum()
+	233168
+	'''
+	
+if __name__ == '__main__':
+	import doctest
+	doctest.testmod()
