c3f12acbb4e867f206559685fb999bccc50cc99a
Nitin Tulswani <tulswani19@gmail.com>
2016-08-23 23:58:24 +0530

    tested

--- problem57.py
+++ problem57.py
@@ -1,14 +1,20 @@
 import fractions
 
 def root_convergents(limit):
-	d = fractions.Fraction(1/2) 
-	for i in range(limit):
-		d = fractions.Fraction(1/(2+d)) # This grows until the iterations end.
-		yield 1 + d 
+    d = fractions.Fraction(1/2) 
+    for i in range(limit):
+        d = fractions.Fraction(1/(2+d)) # This grows until the iterations end.
+        yield 1 + d 
 
 def main():
-	print(len([f for f in root_convergents(1000) if len(str(f.numerator))>
-					len(str(f.denominator))]))
+    '''
+    >>> main()
+    153
+    '''
+
+    print(len([f for f in root_convergents(1000) if len(str(f.numerator))>
+                    len(str(f.denominator))]))
 
 if __name__ == '__main__':
-	main()
+    import doctest
+    doctest.testmod(verbose=True)
